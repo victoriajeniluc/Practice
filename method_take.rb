2c339760b827909d55d4c2c865d52a864735266c
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-22 16:50:10 -0500

    pseudo code

---
+++ method_take.rb
@@ -0,0 +1,14 @@
+# Pseudocode 
+#------------------------------------
+
+	# Create a method that will return back an item starting with the number that is given and ending at the end of the item  
+		# input: array / string, number 
+		# output: array / string 
+			#Use the array and its indexes
+				# Let the number be the starting point of the array/ string 
+				# Let the length be the ending point of the array/ string
+
+	### BASICALLY REDOING THE TAKE METHOD BUT WITH A STARTING POINT 
+
+#------------------------------------
+
