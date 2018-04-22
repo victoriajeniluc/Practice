a8ec3cddab32a62882c43de3d4d4b9b65bd3761f
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-22 16:26:16 -0500

    pseudo code

---
+++ prime_numbers.rb
@@ -0,0 +1,15 @@
+# Pseudocode 
+#----------------------------------------------------
+
+# Create a method that determines a number is prime
+	# input: number 
+	# output: true or false 
+# Create a if / else statement for edge cases 
+		# If a number is less than one or equal to 1
+			# => return FALSE 
+		# If a number is equal to 2
+			# => return TRUE 
+		# If half of the range of numbers is divisble by 2,
+			# =>  return FALSE if not prime, return TRUE if prime 
+
+#----------------------------------------------------
