b6db42a5e6e0ebb4e4b554e765e7c9df11d79c0d
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-22 16:30:30 -0500

    create a method on how to find a number that is prime

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -13,3 +13,17 @@
 			# =>  return FALSE if not prime, return TRUE if prime 
 
 #----------------------------------------------------
+
+def prime?(n)
+	if n <= 1
+		# returning back false because 1 is one of the numbers that is considered not prime 
+		return false 
+	elsif n == 2 
+		# returning back true because 2 is one of the numbers that can only be divided by itself(2) and be greater than 1
+		return true 
+	else 
+		return (2..n/2).none? {|i| n % i == 0 }
+	end 
+end 
+
+puts prime?(2)
