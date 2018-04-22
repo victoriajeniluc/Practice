98fe8ed7a1b61d83f37d868d781451d60311027b
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-22 15:40:45 -0500

    Update reverse.rb

--- reverse.rb
+++ reverse.rb
@@ -13,22 +13,22 @@
 	# Turn the array back into a string 
 # -----------------------
 def split_string(string)
-	string.split("")
+  string.split("")
 end
 
 def rearrange_string(string)
-	string_array = split_string(string)
-	container = []
-	string_array.length.times { container << string_array.pop }
-	container
+  string_array = split_string(string)
+  container = []
+  string_array.length.times { container << string_array.pop }
+  container
 end  
 
 def join_string(string)
-	rearrange_string(string).join("")
+  rearrange_string(string).join("")
 end
 
 def print_reverse_string(string)
-	puts join_string(string)
+  puts join_string(string)
 end  
 
 print_reverse_string("hello")
