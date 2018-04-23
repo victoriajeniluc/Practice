4ecb4cdd6e7216a330faf263515bd866cb99c1e1
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-22 19:46:30 -0500

    pseudocode for an alias manager

--- alias_manager.rb
+++ alias_manager.rb
@@ -1,7 +1,12 @@
 # Pseudocode 
 # -------------------------------------------
 
-# Create a 
-
+# Create a method that will change a user's name 
+	# input: name (string)
+	# output: alias name (string)
+# For every vowel in the name, it will be switched the to next vowel in the line (ex: E will become I, O will become U)
+# For the first name and last name, it will swap them.
+	# If there is a middle name, it will remain in the middle 
+# For each constant in the name it will be the next constant in the alphabet 
 
 # -------------------------------------------
--- reverse.rb
+++ reverse.rb
@@ -13,21 +13,21 @@
 	# Turn the array back into a string 
 # -----------------------
 
-	def split_string(string)
-	  string.split("")
-	end
+def split_string(string)
+  string.split("")
+end
 
-	def rearrange_string(string)
-	  string_array = split_string(string)
-	  container = []
-	  string_array.length.times { container << string_array.pop }
-	  container
-	end  
+def rearrange_string(string)
+  string_array = split_string(string)
+  container = []
+  string_array.length.times { container << string_array.pop }
+  container
+end  
 
-	def join_string(string)
-	  rearrange_string(string).join("")
-	end
+def join_string(string)
+  rearrange_string(string).join("")
+end
 
-	def print_reverse_string(string)
-	  puts join_string(string)
-	end  
+def print_reverse_string(string)
+  puts join_string(string)
+end  
