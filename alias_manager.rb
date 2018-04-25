d9d88a1410a2ae6fc5d630ab637f15831c88e001
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-24 23:05:26 -0500

    create a alias manager with a rspec test

--- alias_manager.rb
+++ alias_manager.rb
@@ -4,9 +4,62 @@
 # Create a method that will change a user's name 
 	# input: name (string)
 	# output: alias name (string)
-# For every vowel in the name, it will be switched the to next vowel in the line (ex: E will become I, O will become U)
+
+
 # For the first name and last name, it will swap them.
 	# If there is a middle name, it will remain in the middle 
-# For each constant in the name it will be the next constant in the alphabet 
+		# Break the name into an array and swap the first and last name and join it back into a string 
+			# input: name (string)
+			# output: swapped name (string)
+
+# For every vowel in the name, it will be switched the to next vowel in the line (ex: E will become I, O will become U)
+	# Create a loop that will search through the whole name and the whole vowel string
+	# If the vowel is found, it will be inserted into the alias name string 
+
+# For each constant, the name it will be the next constant in the alphabet 
+	# Create a loop that will search through the whole name and the whole alphabet string 
+	# If the constant is found, it will be inserted into the alias name string 
 
 # -------------------------------------------
+
+def breaking_name(name)
+	name.split(" ")
+end 
+
+def swapping_name(name)
+	name_array = breaking_name(name)
+	name_array[0], name_array[name_array.length-1] = name_array[name_array.length-1], name_array[0]
+	name_array.join(" ")
+end 
+
+def changing_name(name)
+	name_characters = swapping_name(name).chars 
+
+	name_characters.map do |character| 
+		if character == "a"
+			"e"			
+		elsif character == "e"
+			"i"
+		elsif character == "i"
+			"o"
+		elsif character == "o"
+			"u"
+		elsif character == "u" 
+			"a"
+		elsif character == " " 
+			" "
+		else 
+			character.next
+		end 		
+	end 
+end 
+
+def joining_name(name)
+	changing_name(name).join("")
+end 
+
+def printing_name(name)
+	joining_name(name)
+end
+
+printing_name("victoria jeni luc")
--- alias_manager_spec.rb
+++ alias_manager_spec.rb
@@ -1,7 +1,19 @@
 require 'rspec'
 require './alias_manager'
 
-describe Alias do 
-	
-	
+describe "alias" do
+	it "will break the name given into an array" do 
+		expect(breaking_name("Victoria Jeni Luc")).to eq(["Victoria", "Jeni", "Luc"])
+	end  
+
+	it "will swap the first name and the last name" do 
+		expect(swapping_name("Victoria Jeni Luc")).to eq("Luc Jeni Victoria")
+	end 
+
+	it "will change each character in the name" do 
+		expect(changing_name("Victoria Jeni Luc")).to eq(["M", "a", "d", " ", "K", "i", "o", "o", " ", "W", "o", "d", "u", "u", "s", "o", "e"])
+	end 
+	it "will change the vowels in the name to the next vowel" do 
+		expect(printing_name("Victoria Jeni Luc")).to eq("Mad Kioo Woduusoe")
+	end 
 end 
