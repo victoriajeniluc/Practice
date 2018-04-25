b8c46fd810257ae1a1ebf10d04e483e93c9bbc34
Victoria Luc <victoriajeniluc@gmail.com>
2018-04-25 14:34:13 -0500

    pseudcode how to group anagrams

---
+++ group_anagrams.rb
@@ -0,0 +1,12 @@
+# Pseudocode 
+# -------------------------------------------
+
+# Given an array of strings, group anagrams together. 
+
+# Create a method that will take the array
+	# input: array 
+	# out put: 2D array 
+
+# Create a loop that will match the first word with the next anagram. Once they are grouped together, shove them into an array. Repeat until the end of the original array. 
+
+# -------------------------------------------
---
+++ group_anagrams_spec.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require './group_anagrams'
+
+describe "group_anagrams" do
+
+end 
