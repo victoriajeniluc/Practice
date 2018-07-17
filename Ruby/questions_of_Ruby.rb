1e09956497a71c8d748e0dc05594016b0fca3063
Victoria Luc <victoriajeniluc@gmail.com>
2018-07-16 21:13:05 -0500

    objects have methods

--- Ruby/questions_of_Ruby.rb
+++ Ruby/questions_of_Ruby.rb
@@ -1,5 +1,7 @@
+
 # Object-oriented programming 
 	# Classes are like ideas, objects are concrete things, manifestations of their ideas 
+#--------------------------------------------------------------------------
 
 # Variables 
 	#EXAMPLE 1
@@ -7,31 +9,47 @@
 	# => I have created a variable named "number". I have assigned number to 1. "Number" is a name for an object. 
 	# => '=' is an assignment operator  
 
-	#EXAMPLE@
+	#EXAMPLE
 	number = 2 + 3 * 4 
 	# => number = 14 
 	# => Ruby evaluates the expression on the right first then assigns it to the name number. 
 
+#--------------------------------------------------------------------------
 
 # Objects / Classes / Methods 
+	# An object is an instance of its class 
+	# When an object is created from a class, the class is instantiated. 
+		#^^^ What does this mean? 
+			# Classes have a bunch of characteristics, but most importantly, every class defines a number of methods, which is specific to this type of thing
+			# Objects inherit methods from their classes. 
+
+	#-----------------------------
+	# What is a class? 
+		# => Text-book answer: classes are a blue-print for constructing computer models for real / virtual objects 
+		# => Reality: classes hold data, have methods that interact with the data, and are used to instantiate objects 
+	#-----------------------------
+	# Objects have methods 
+		# Methods are an object's behavior 
+			# AKA "An object's methods are things that the object can do"
+
+		#EXAMPLE 
+			# => Think about a person, like a friend of yours. You can ask this person for their name(call a method), and they'll respond back with their name(return it to you). Their name is a piece of knowledge of this person has and the ability to tell you (respond to your question) is a piece of behavior (a method) they have. 
+
+		# Methods add behavior that is useful to have for a particular type of object.
+	#-----------------------------
 
 
-# What is a class? 
-	# => Text-book answer: classes are a blue-print for constructing computer models for real / virtual objects 
-	# => Reality: classes hold data, have methods that interact with the data, and are used to instantiate objects 
+		#EXAMPLE OF OBJECTS / CLASSES / METHODS 
+		class WhatAreClasses 
+			def initialize 
+				@data = "I'm an instance data of this object. Hello."
+			end 
 
-	#EXAMPLE 
-
-	class WhatAreClasses 
-		def initialize 
-			@data = "I'm an instance data of this object. Hello."
+			def method 
+				puts @data.gsub("instance", "altered")
+			end
 		end 
 
-		def method 
-			puts @data.gsub("instance", "altered")
-		end
-	end 
-
-	object = WhatAreClasses.new
-	object.method 
-	# => I'm altered data of this object. Hello. 
+		object = WhatAreClasses.new
+		object.method 
+		# => I'm altered data of this object. Hello. 
